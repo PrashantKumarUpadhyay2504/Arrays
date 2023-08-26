9113e5e91b8ba91eb70c76a4face460261c15221
Prashant Kumar Upadhyay <pkumarupadhyay0@gmail.com>
2023-08-26 17:28:08 +0530

    new Commit

---
+++ src/Anagram.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);        
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        char chr1[]= str1.toCharArray();
+        char chr2[]= str2.toCharArray();
+
+        if(str1.length()!=str2.length()){
+            System.out.println("Not anagrams");
+        }else{
+            int flag=0;
+            for(int i=0 ; i<str1.length(); i++){
+                for(int j=0 ; j<str2.length() ; j++){
+                    if(chr1[i]==chr2[j]){
+                        flag=1;
+                    }else{
+                        continue;
+                    }
+                }
+            }
+            if(flag==1){
+                System.out.println("Anagrams");
+            }else{
+                System.out.println("Not Anagram");
+            }
+        }
+    }
+}
---
+++ src/Array.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+
+        for(int i =0 ; i< n; i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
