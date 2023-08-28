c61d8fe45ab04a43cd35741f1d84ab506f96eeef
Prashant Kumar Upadhyay <pkumarupadhyay0@gmail.com>
2023-08-28 17:03:53 +0530

    commit

--- src/String/Anagram.java
+++ src/String/Anagram.java
@@ -1,32 +1,34 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Anagram {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);        
-        String str1 = sc.nextLine();
-        String str2 = sc.nextLine();
 
-        char chr1[]= str1.toCharArray();
-        char chr2[]= str2.toCharArray();
+  public static void main(String[] args) {
 
-        if(str1.length()!=str2.length()){
-            System.out.println("Not anagrams");
-        }else{
-            int flag=0;
-            for(int i=0 ; i<str1.length(); i++){
-                for(int j=0 ; j<str2.length() ; j++){
-                    if(chr1[i]==chr2[j]){
-                        flag=1;
-                    }else{
-                        flag=0;
-                    }
-                }
-            }
-            if(flag==1){
-                System.out.println("Anagrams");
-            }else{
-                System.out.println("Not Anagram");
-            }
-        }
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter first String: ");
+    String str1 = input.nextLine();
+    System.out.print("Enter second String: ");
+    String str2 = input.nextLine();
+
+    if(str1.length() == str2.length()) {
+
+      char[] charArray1 = str1.toCharArray();
+      char[] charArray2 = str2.toCharArray();
+
+      Arrays.sort(charArray1);
+      Arrays.sort(charArray2);
+
+      boolean result = Arrays.equals(charArray1, charArray2);
+
+      if(result) {
+        System.out.println(str1 + " and " + str2 + " are anagram.");
+      }
+      else {
+        System.out.println(str1 + " and " + str2 + " are not anagram.");
+      }
     }
+    else {
+      System.out.println(str1 + " and " + str2 + " are not anagram.");
+    }
+  }
 }
