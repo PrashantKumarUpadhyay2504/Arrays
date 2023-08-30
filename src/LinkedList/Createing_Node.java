3fb92266b526e6447c6bf972310d03218ba84b7c
Prashant Kumar Upadhyay <pkumarupadhyay0@gmail.com>
2023-08-30 16:02:14 +0530

    commit

---
+++ src/LinkedList/Createing_Node.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+
+class Node{
+    Node next;
+    int data;
+
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public class Createing_Node {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+
+    }
+    
+}
