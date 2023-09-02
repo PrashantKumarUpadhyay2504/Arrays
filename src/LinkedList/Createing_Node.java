29d44943c974bdf786b22d109951ca75adcb8d66
Prashant Kumar Upadhyay <pkumarupadhyay0@gmail.com>
2023-09-02 19:30:25 +0530

    commit

--- src/LinkedList/Createing_Node.java
+++ src/LinkedList/Createing_Node.java
@@ -1,19 +1,30 @@
 package LinkedList;
 
 
-class Node{
-    Node next;
-    int data;
+class Node <T>{
+    Node <T> next;
+    T data;
 
-    Node(int data){
+    Node(T data){
         this.data=data;
         this.next=null;
     }
 }
 public class Createing_Node {
     public static void main(String[] args) {
-        Node head = new Node(10);
+        Node Node1 = new Node(10);
+        Node Node2 = new Node(20);
+        Node Node3 = new Node(30);
+        Node Node4 = new Node(40);
 
+        Node1.next=Node2;
+        Node2.next = Node3;
+        Node3.next=Node4;
+        Node head=Node1;
+        while(head.next!=null){
+            System.out.print(head.data+" ");
+            head=head.next;
+        }
     }
     
 }
