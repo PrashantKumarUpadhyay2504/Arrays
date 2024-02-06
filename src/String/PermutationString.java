005373adf338bfa86a0c706b19920fe357c12019
Prashant Kumar Upadhyay <pkumarupadhyay0@gmail.com>
2024-02-06 20:52:26 +0530

    commit

--- src/String/PermutationString.java
+++ src/String/PermutationString.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class PermutationString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first String : ");
         String str1=sc.next();
+        System.out.println("Enter second String : ");
         String str2=sc.next();
 
         if(str1.length() == str2.length()){
